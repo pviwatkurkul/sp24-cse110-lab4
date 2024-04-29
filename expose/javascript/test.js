6769ce4893425d5b580cba13a2996e4d49eeef66
pviwatkurkul <pviwatkurkul@ucsd.edu>
2024-04-28 23:48:02 -0700

    added part 1 files

--- expose/javascript/test.js
+++ expose/javascript/test.js
@@ -1,12 +1,1 @@
-function sumValues(num1, num2, add){
-    if(add){
-        var result = 0;
-        result = num1+num2;
-        console.log('values adeded', result);
-    }
-    else return;
-
-    console.log('final result: ', result);
-}
-
-sumValues(10,10,true);
+console.log(true === Boolean(2));
